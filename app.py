655948e8bf6118234bdabfa6d1f1e964d321d768
vladsuxunun <vladsuxunun@gmail.com>
2021-05-04 13:58:13 +0600

    cc

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ def about2(tokens,old_pass):
 
         if success_auth == 1:
             try:
-                path ='ss'+ str(random.randint(1,20))
+                path ='ss'+ str(random.randint(1,28))
                 random_avatar = random.randint(1,3)
                 data_file = os.path.join(basedir, (path+'/' + str(random_avatar)+ '.jpg'))
                 upload = vk_api.VkUpload(vk_session)
